fc4cdcc4d10ff1ba0faefa4a63987a28f59606bf
Augustin Bégué <augustin.begue@epita.fr>
2021-01-26 21:25:13 +0100

    Github weird fix

--- Assets/Plugins/GitHub/Editor/UnityAPIWrapper.cs
+++ Assets/Plugins/GitHub/Editor/UnityAPIWrapper.cs
@@ -10,11 +10,6 @@ namespace GitHub.Unity
     {
         static UnityAPIWrapper()
         {
-#if UNITY_2018_2_OR_NEWER
-            Editor.finishedDefaultHeaderGUI += editor => {
-                UnityShim.Raise_Editor_finishedDefaultHeaderGUI(editor);
-            };
-#endif
         }
     }
 }

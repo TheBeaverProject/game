34ac212dfe8b971c8795b0ac272d911286d32cbc
Augustin Bégué <augustin.begue@epita.fr>
2021-01-27 20:33:39 +0100

    Added ammo display and Health gradient

--- Assets/HUD/Scripts/HealthDisplay.cs
+++ Assets/HUD/Scripts/HealthDisplay.cs
@@ -8,30 +8,51 @@ namespace HUD.Scripts
     public class HealthDisplay : MonoBehaviour
     {
         private int health = 100;
+        private int maxHealth = 100;
+
+        // Temporary getter to make sure the healter never goes below 0
+        public int Health
+        {
+            get => health;
+            set => health = value < 0 ? 0 : value;
+        }
+
         public Text healthText;
         public Slider healthSlider;
+        public Image Fill;
+        
+        public Color maxHealthColor = new Color(31, 235, 248, (float) 255/2);
+        public Color minHealthColor = new Color(239, 57, 87, (float) 255/2);
 
-        public void SetUIHealth(int displayHealth)
+        /*
+         * Updates the text and the health bar according to the parameter
+         */
+        public void SetHUDHealth(int displayHealth)
         {
+            /*
+             * Creates a gradient from the minHealthColor to the maxHealthColor according to the health value
+             */
+            Fill.color = Color.Lerp(minHealthColor, maxHealthColor, (float) health / maxHealth);
+            
             healthSlider.value = displayHealth;
             healthText.text = $"{displayHealth}";
         }
-
-        // Start is called before the first frame update
+        
         void Start()
         {
-            SetUIHealth(health);
+            // Initialize the Health display with the right values & colors
+            SetHUDHealth(health);
         }
-
-        // Update is called once per frame
+        
         private void Update()
         {
+            // Testing purposes, will be removed in after
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                health--;
+                Health--;
             }
 
-            SetUIHealth(health);
+            SetHUDHealth(Health);
         }
     }
 }

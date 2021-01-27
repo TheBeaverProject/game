68c7625792e54843a6fee33f080647b1511cb9da
Baptiste Fontaine <baptiste.fontaine@epita.fr>
2021-01-27 18:38:11 +0100

    Dash mechanic implemented

---
+++ Assets/Dash.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Dash : MonoBehaviour
+{
+    public Vector3 dash;
+    public const float maxDashTime = 2.5f;
+    public float dashDistance = 5f;
+    public float dashStoppingSpeed = 0.1f;
+    float currentDashTime = maxDashTime;
+    private float dashSpeed = 6;
+    public CharacterController controller;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            currentDashTime = 0;
+        }
+
+        if (currentDashTime < maxDashTime)
+        {
+            dash = transform.forward * dashDistance;
+            currentDashTime += dashStoppingSpeed;
+        }
+        else
+        {
+            dash = Vector3.zero;
+        }
+
+        controller.Move(dash * Time.deltaTime * dashSpeed);
+    }
+}

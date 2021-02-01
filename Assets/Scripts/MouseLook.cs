e5ca0e7fe5af368990a26b4f1125ff8c0ed10df2
François Houssin <francois1.houssin@epita.fr>
2021-02-01 02:39:01 +0100

    FIX issue #11: Player can look though model

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -23,10 +23,20 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
         //Get mouse input
-        
+
+        // If we rotate with mouseY we end up rotating toward the wrong direction
         xRotation -= mouseY;
-        //- because if + then it moves in the opposite direction
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        /**
+        *   Set max rotation on Y axis 
+        *   as in real life you can't look more than 90 degrees up or down
+        *   Settings the encadrement endup to the following issue:
+        *   https://github.com/TheBeaverProject/game/issues/11#issue-797167972
+        *
+        *   FIX: As in real life, if you look 90degrees down you look though your neck :)
+        *        set max degree down to 45 (it can be a bit reduce but it match the model)  
+        **/
+        xRotation = Mathf.Clamp(xRotation, -90f, 45f);
         //-90 and 90 to not allow the player to move too fast and see behind him
         
         transform.localRotation = Quaternion.Euler(xRotation,0f,0f);

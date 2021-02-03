63ad4f5b8ece885d37a2c068e75904503aaa2bd8
Augustin Bégué <augustin.begue@epita.fr>
2021-02-03 11:59:20 +0100

    start of the main menu logic (don't worry, its ugly af)

---
+++ Assets/Scripts/UI/MainMenu/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace UI.MainMenu
+{
+    public class MenuController : MonoBehaviour
+    {
+        public void AccountHandler()
+        {
+            
+        }
+
+        public void SoloHandler()
+        {
+            SceneManager.LoadScene("Scenes/InGameHud");
+        }
+
+        public void MultiplayerHandler()
+        {
+            SceneManager.LoadScene("Scenes/InGameHud");
+        }
+
+        public void SettingsHandler()
+        {
+            
+        }
+
+        public void QuitHandler()
+        {
+            Application.Quit();
+        }
+    }
+    
+}

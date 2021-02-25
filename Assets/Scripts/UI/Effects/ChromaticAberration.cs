35e75de179cd98659dc2002ee0d7bd1f383ed198
Augustin Bégué <augustin@begue.cc>
2021-02-25 10:49:16 +0100

    Added chromatic aberation shader

---
+++ Assets/Scripts/UI/Effects/ChromaticAberration.cs
@@ -0,0 +1,52 @@
+﻿namespace UI.Effects
+{
+    using UnityEngine;
+    using System.Collections;
+    
+    public class ChromaticAberration : MonoBehaviour {
+   
+        #region Variables
+        public Shader curShader;
+        public float ChromaticAbberation = 1.0f;
+        private Material curMaterial;
+        #endregion
+   
+        #region Properties
+        
+        Material material
+        {
+            get
+            {
+                if(curMaterial == null)
+                {
+                    curMaterial = new Material(curShader);
+                    curMaterial.hideFlags = HideFlags.HideAndDontSave; 
+                }
+                return curMaterial;
+            }
+        }
+        
+        #endregion
+
+        void OnRenderImage (RenderTexture sourceTexture, RenderTexture destTexture)
+        {
+            if(curShader != null)
+            {
+                material.SetFloat("_AberrationOffset", ChromaticAbberation);
+                Graphics.Blit(sourceTexture, destTexture, material);
+            }
+            else
+            {
+                Graphics.Blit(sourceTexture, destTexture); 
+            }
+        }
+
+        void OnDisable ()
+        {
+            if(curMaterial)
+            {
+                DestroyImmediate(curMaterial); 
+            }
+        }
+    }
+}

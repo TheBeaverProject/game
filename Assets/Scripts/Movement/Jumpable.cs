b34739b803e675f78dcaef0c39fdc03734de057e
François Houssin <francois1.houssin@epita.fr>
2021-02-26 18:09:37 +0100

    Movement interface implementation

---
+++ Assets/Scripts/Movement/Jumpable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace Jumps 
+{
+    public interface IJumpable
+    {
+        Vector3 Jump();
+    }
+
+    /**
+     * E.G jump implementation example
+     */
+    public class JumpBase : IJumpable
+    {
+        public Vector3 Jump()
+        {
+            // TODO: store it in const instead of hardcoding it
+            return new Vector3(0, (float) Math.Sqrt(2f * -2f * -9.81f), 0);
+        }
+    }
+
+    public class JumpDouble : IJumpable
+    {
+        public Vector3 Jump()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class JumpNone : IJumpable
+    {
+        public Vector3 Jump()
+        {
+            return new Vector3(0, 0, 0);
+        }
+    }
+}

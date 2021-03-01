e3c1eece83617efa80f9efdba5f02ad139cff2a1
Baptiste Fontaine <baptiste.fontaine@epita.fr>
2021-03-01 18:26:07 +0100

    Grenades added to implement on camera

---
+++ Assets/Scripts/Guns/Grenade.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Photon.Pun;
+using PlayerManagement;
+using UnityEngine;
+
+namespace Guns
+{
+    public class Grenade : MonoBehaviourPun
+    {
+        private float delay = 3f;
+        private float countDown;
+        private bool hasExploded;
+        private float radius = 5f;
+        private float force = 5f;
+        //private GameObject effect;
+
+        private void Start()
+        {
+            countDown = delay;
+        }
+
+        private void Update()
+        {
+            countDown -= Time.deltaTime;
+            if (countDown <= 0f && !hasExploded)
+            {
+                Explode();
+                hasExploded = true;
+            }
+        }
+
+        private void Explode()
+        {
+            Debug.Log("Grenade Explosion");
+            //Instantiate(effect, transform.position, transform.rotation);
+
+            Collider[] colliders =Physics.OverlapSphere(transform.position, radius);
+
+            foreach (Collider hit in colliders)
+            {
+                Rigidbody rb = hit.GetComponent<Rigidbody>();
+                if (hit.TryGetComponent<PlayerManager>(out PlayerManager damagedPlayer))
+                {
+                    rb.AddExplosionForce(force,transform.position,radius);
+                }
+            }
+            
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Guns/GrenadeThrow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Photon.Pun;
+using UnityEngine;
+
+namespace Guns
+{
+    public class GrenadeThrow : MonoBehaviourPun
+    {
+        private float throwForce = 20f;
+        private GameObject grenadePrefab;
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.G))
+            {
+                ThrowGrenade();
+            }
+        }
+
+        private void ThrowGrenade()
+        {
+            GameObject grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
+            Rigidbody rb = grenade.GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * throwForce, ForceMode.VelocityChange);
+        }
+    }
+}

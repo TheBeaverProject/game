b8d88cd328f32fd84b8f3af9f52637a074e2216c
Baptiste Fontaine <baptiste.fontaine@epita.fr>
2021-04-11 21:29:49 +0200

    First IA training very much in progress

---
+++ Assets/Scripts/AI/MoveToGoalAgent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using UnityEngine.XR.WSA.Input;
+
+public class MoveToGoalAgent : Agent
+{
+    public override void OnEpisodeBegin()
+    {
+        transform.position = new Vector3(2.330062f, 2.04f, -4.941737f);
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        float moveX = vectorAction[0];
+        float moveZ = vectorAction[1];
+        float moveSpeed = 3f;
+        transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+    [SerializeField] private Transform targetTransform;
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(targetTransform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Goal>(out Goal goal))
+        {
+            SetReward(+1f);
+            EndEpisode();
+        }
+        if (other.TryGetComponent<Penalty>(out Penalty penalty))
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        float[] continuousActions = actionsOut;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+}
